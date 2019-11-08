5741adf69d68205a81ae565b3ed9c85ecc0773ed
Trevor Alex <pascerveau@gmail.com>
2019-11-08 14:06:29 -0700

    trap

---
+++ Assets/Scripts/Tiles/Trap.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : Tile
+{
+    [SerializeField]
+    private bool m_spikeUp;
+    public GameObject model;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void OnValidate()
+    {
+        UpdateSpike();
+    }
+
+    private void UpdateSpike()
+    {
+        if (m_spikeUp)
+        {
+            model.transform.position = transform.position;
+        }
+        else
+        {
+            model.transform.position = transform.position -
+                new Vector3(0, VII.GameData.STEP_SIZE / 2, 0);
+        }
+    }
+
+    protected override void OnTickEnd()
+    {
+        m_spikeUp = !m_spikeUp;
+        UpdateSpike();
+        base.OnTickEnd();
+    }
+
+    protected override void OnPlayerEnter(Player player)
+    {
+        base.OnPlayerEnter(player);
+        if (m_spikeUp)
+        {
+            player.Respawn();
+        }
+    }
+
+    protected override void OnPlayerExit(Player player)
+    {
+        base.OnPlayerExit(player);
+    }
+}
