503882102e06487a4bd77b140615680821ac3b75
Qingqi <qingqigao12@gmail.com>
2019-11-15 14:56:53 -0600

    initial save code

---
+++ Assets/Scripts/SaveLoad/SavePlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SavePlayerData
+{
+    public Vector3 respawnPoint;
+    //public string playerName;
+    
+    public SavePlayerData(Player player)
+    {
+        respawnPoint = player.PlayerData.respawnPosition;
+    }
+}
---
+++ Assets/Scripts/SaveLoad/TriggerSave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSave : Tile
+{
+    
+    public void SavePlayer(Player player)
+    {
+
+        SaveSystem.SavePlayer(player);
+    }
+
+    public void LoadPlayer(Player player)
+    {
+        SavePlayerData data = SaveSystem.LoadPlayer();
+        player.PlayerData.respawnPosition = data.respawnPoint;
+    }
+    private void Update()
+    {
+        
+    }
+}
