09ac0be8d89b2c05e467f79abfa847e17b2695c2
Xiubo <zhaoxiubo144@gmail.com>
2019-11-20 22:38:18 -0700

    raycast

--- Assets/Scripts/Tiles/Lava.cs
+++ Assets/Scripts/Tiles/Lava.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ground_detector;
+    private float m_maxCastDistance = 1f;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            LavaSpread();
+    }
+
+    void LavaSpread()
+    {
+        if (CheckAbutTile(transform.forward))
+            Debug.Log("Forward hit");
+        if (CheckAbutTile(-transform.forward))
+            Debug.Log("Backward hit");
+        if (CheckAbutTile(transform.right))
+            Debug.Log("Right hit");
+        if (CheckAbutTile(-transform.right))
+            Debug.Log("Left hit");
+    }
+
+    bool CheckAbutTile(Vector3 i_dir)
+    {
+        RaycastHit hit;
+        bool hitResult;
+        hitResult = Physics.Raycast(ground_detector.transform.position, i_dir, out hit, m_maxCastDistance);
+        if(hitResult)
+        {
+            //Debug.Log(hit.transform.name);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
