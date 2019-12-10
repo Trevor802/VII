0cdd0fdad3352806a85e52235cbe52ba24597799
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2019-12-10 15:47:46 -0700

    imported steamworks package & connected to steam with steamid & achievement working with steam

---
+++ Assets/Scripts/Achievements/SteamAchievements.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamAchievements : MonoBehaviour
+{
+    public bool achievementUnlocked;
+
+    private void Start()
+    {
+        //unlockAchievement("achievement_00");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            unlockAchievement("achievement_00");
+            print("unlocked");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            lockAchievement("achievement_00");
+            print("locked");
+        }
+        SteamAPI.RunCallbacks();
+    }
+
+    public void unlockAchievement(string achievementID)
+    {
+        getAchievementStatus(achievementID);
+        if (!achievementUnlocked)
+        {
+            SteamUserStats.SetAchievement(achievementID);
+            SteamUserStats.StoreStats();
+        }
+    }
+
+    void lockAchievement(string achievementID)
+    {
+        getAchievementStatus(achievementID);
+        if (achievementUnlocked)
+        {
+            SteamUserStats.ClearAchievement(achievementID);
+            SteamUserStats.StoreStats();
+        }
+    }
+
+    void getAchievementStatus(string achievementID)
+    {
+        SteamUserStats.GetAchievement(achievementID, out achievementUnlocked);
+    }
+}
