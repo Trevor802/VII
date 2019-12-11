fc74e25dd06e068871b8cc1d93c69b89073bdc39
Trevor Alex <pascerveau@gmail.com>
2019-12-10 23:22:07 -0700

    Clear cache

---
+++ Assets/Scripts/Core/AnimationCallback.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationCallback : MonoBehaviour
+{
+    public void OnAnimationCallback(AnimationEvent i_event)
+    {
+        SendMessage(i_event.stringParameter);
+    }
+
+}
