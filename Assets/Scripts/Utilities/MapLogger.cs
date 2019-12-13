b01e1561cee5f235f8eb8b5e9a39792b9de4fb7c
Trevor Alex <pascerveau@gmail.com>
2019-12-13 10:53:39 -0700

    Install JSONObject, update map logger implementation

--- Assets/Scripts/Utilities/MapLogger.cs
+++ Assets/Scripts/Utilities/MapLogger.cs
@@ -12,11 +12,25 @@ public class LevelLogData
     public int remainingLives;
     public int remainingSteps;
     public float levelElapsedTime;
+    private JSONObject m_JSON = new JSONObject(JSONObject.Type.OBJECT);
 
     public LevelLogData(int i_remainingLives, int i_remainingSteps,
-        float i_levelElapsedTime, string i_levelName) =>
-        (remainingLives, remainingSteps, levelElapsedTime, levelName) =
-        (i_remainingLives, i_remainingSteps, i_levelElapsedTime, i_levelName);
+        float i_levelElapsedTime, string i_levelName)
+    {
+        levelName = i_levelName;
+        remainingLives = i_remainingLives;
+        remainingSteps = i_remainingSteps;
+        levelElapsedTime = i_levelElapsedTime;
+        m_JSON.AddField("LevelName", this.levelName);
+        m_JSON.AddField("RemainingLives", this.remainingLives);
+        m_JSON.AddField("RemainingSteps", this.remainingSteps);
+        m_JSON.AddField("LevelElapsedTime", this.levelElapsedTime);
+    }
+
+    public JSONObject GetJSONObject()
+    {
+        return m_JSON;
+    }
 }
 
 [Serializable]
@@ -31,16 +45,27 @@ public class MapLogger : MonoBehaviour
     [SerializeField]
     private float m_elapsedTime;
     [SerializeField]
-    private string m_levelDataJson;
     private float m_levelElapsedTime;
     private int m_levelIndex;
     private List<LevelLogData> m_levelData = new List<LevelLogData>();
+    private JSONObject m_JSON = new JSONObject(JSONObject.Type.OBJECT);
+    private JSONObject m_LevelJSONArray = new JSONObject(JSONObject.Type.ARRAY);
 
     private void Awake()
     {
+        JSONSetFields(m_JSON);
         m_mapName = SceneManager.GetActiveScene().name;
         m_startTime = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
         VII.VIIEvents.LevelFinish.AddListener(OnLevelFinish);
+    }
+
+    private void JSONSetFields(JSONObject i_JSONObject)
+    {
+        i_JSONObject.SetField("MapName", m_mapName);
+        i_JSONObject.SetField("StartTime", m_startTime);
+        i_JSONObject.SetField("EndTime", m_endTime);
+        i_JSONObject.SetField("ElapsedTime", m_elapsedTime);
+        i_JSONObject.SetField("LevelData", m_LevelJSONArray);
     }
 
     private void Update()
@@ -51,19 +76,17 @@ public class MapLogger : MonoBehaviour
     private void OnDestroy()
     {
         m_endTime = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
-        m_levelDataJson = JsonHelper.ToJson(m_levelData.ToArray());
-        string data = JsonUtility.ToJson(this);
-        data = data.Replace("\\", "");
-        data = data.Replace("\"{\"Items\":", "");
-        data = data.Replace("}\"", "");
-        LogData(data);
+        JSONSetFields(m_JSON);
+        LogData(m_JSON.ToString());
     }
 
     private void OnLevelFinish(Player i_player)
     {
         m_levelElapsedTime = m_elapsedTime - m_levelElapsedTime;
-        m_levelData.Add(new LevelLogData(i_player.GetLives(), i_player.GetSteps(),
-            m_levelElapsedTime, m_levelIndex++.ToString()));
+        LevelLogData levelLog = new LevelLogData(i_player.GetLives(), i_player.GetSteps(),
+            m_levelElapsedTime, m_levelIndex++.ToString());
+        m_levelData.Add(levelLog);
+        m_LevelJSONArray.Add(levelLog.GetJSONObject());
     }
 
     private void LogData(string i_data)
