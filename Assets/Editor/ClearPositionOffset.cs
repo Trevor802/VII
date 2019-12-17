83983583ffa629481fa2b264af735965219f04c6
Trevor Alex <pascerveau@gmail.com>
2019-12-17 15:00:04 -0700

    reset map offset to zero

---
+++ Assets/Editor/ClearPositionOffset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class ClearPositionOffset : EditorWindow
+{
+	[MenuItem("Tools/Clear Position Offset")]
+	static void CreateReplaceWithPrefab()
+	{
+		EditorWindow.GetWindow<ClearPositionOffset>();
+	}
+
+	private void OnGUI()
+	{
+		if (GUILayout.Button("Clear Offset"))
+		{
+			var selection = Selection.gameObjects;
+
+			for (var i = selection.Length - 1; i >= 1; --i)
+			{
+				var selected = selection[i];
+
+                selected.transform.localPosition -= selection[0].transform.localPosition;
+                Undo.RecordObject(selected.transform, "Previous transform");
+			}
+            selection[0].transform.localPosition = Vector3.zero;
+		}
+
+		GUI.enabled = false;
+		EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
+	}
+}
