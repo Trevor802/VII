b5900c57ed303adc1d972b6c3da8ee10e3e00bd2
Xiubo <906672365@qq.com>
2019-12-31 14:59:05 +0800

    Rotating Crystal

---
+++ Assets/Scripts/CrystalRotating.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalRotating : MonoBehaviour
+{
+    public float rotateSpeed = 100;
+    private bool hasCrystal = false;
+    public GameObject crystalModel;
+    // Update is called once per frame
+    private void Awake()
+    {
+        DeactivateCrystal();
+    }
+    void Update()
+    {
+        if(hasCrystal)
+        {
+            this.transform.Rotate(this.transform.up, rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ActivateCrystal()
+    {
+        crystalModel.GetComponent<Renderer>().enabled = true;
+        hasCrystal = true;
+    }
+
+    public void DeactivateCrystal()
+    {
+        crystalModel.GetComponent<Renderer>().enabled = false;
+        hasCrystal = false;
+    }
+}
