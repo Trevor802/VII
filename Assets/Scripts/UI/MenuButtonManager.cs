629b54cec78ffbaf36e949a9f3b4772155944501
jieyina <nasjieyi@gmail.com>
2020-01-07 20:48:38 -0700

    check if there is savadata in menu

---
+++ Assets/Scripts/UI/MenuButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonManager : MonoBehaviour
+{
+    public GameObject StartButton;
+    public GameObject RestartButton;
+    public GameObject ContinueButton;
+    public GameObject SeperateLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SaveSystem.LoadPlayer() != null)
+        {
+            StartButton.SetActive(false);
+            RestartButton.SetActive(true);
+            SeperateLine.SetActive(true);
+            ContinueButton.SetActive(true);
+            VII.SceneManager.instance.hasSave = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
