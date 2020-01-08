9acf72a30a6cba0d9612f41e83edb3fb01d09acc
Xiubo <zhaoxiubo144@gmail.com>
2020-01-07 17:27:18 -0700

    Call out in game menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool menuDisplayed;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        menuDisplayed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !menuDisplayed)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            menuDisplayed = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && menuDisplayed)
+        {
+            pauseMenu.SetActive(false);
+            menuDisplayed = false;
+            Time.timeScale = 1f;
+        }
+    }
+}
