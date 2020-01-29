a106412e18f2270298f9a10db449cda93be6ea29
Trevor Alex <pascerveau@gmail.com>
2020-01-29 12:41:47 -0700

    MainMenu resolution adjust

---
+++ Assets/Scripts/UI/LocalizationText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LocalizationText : MonoBehaviour
+{
+    public string[] contents;
+
+    private void Awake()
+    {
+        VII.VIIEvents.LanguageSwitch.AddListener(UpdateUI);
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<Text>().text = contents[(int)LocalizationManager.Instance.Language];
+    }
+
+    private void UpdateUI(VII.Language i_language)
+    {
+        GetComponent<Text>().text = contents[(int)i_language];
+    }
+}
